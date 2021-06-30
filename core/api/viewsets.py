740b815c114be716e12c2762c3c9f5ddcbfeadb0
Rodrigo Klaes <rodrigoklaes@gmail.com>
2021-06-29 21:47:09 -0300

    add filters

--- core/api/viewsets.py
+++ core/api/viewsets.py
@@ -1,10 +1,13 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.filters import SearchFilter
 from core.models import TuristicPoint
 from .serializers import TuristicPointSerializer
 
 
 class TuristicPointViewSet(ModelViewSet):
     serializer_class = TuristicPointSerializer
+    filter_backends = (SearchFilter,)
+    search_fields = ("name", "description")
 
     def get_queryset(self):
         return TuristicPoint.objects.filter(is_approved=True)
